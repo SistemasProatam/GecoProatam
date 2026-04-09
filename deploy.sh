a37628c4dde0ff5ea3f9a89f144bf02db5a3dba1
SistemasProatam <sistemas@proatam.com>
2026-04-09 08:59:46 -0500

    fix: mover logica de versionado a script bash

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Leer versión actual
+VERSION=$(grep "APP_VERSION" version.php | grep -oP "\d+\.\d+\.\d+")
+MAJOR=$(echo $VERSION | cut -d. -f1)
+MINOR=$(echo $VERSION | cut -d. -f2)
+PATCH=$(echo $VERSION | cut -d. -f3)
+
+# Leer tipo de commit
+COMMIT_MSG=$(git log -1 --pretty=%s)
+COMMIT_TYPE=$(echo $COMMIT_MSG | cut -d: -f1 | tr '[:upper:]' '[:lower:]')
+
+echo "Commit tipo: $COMMIT_TYPE"
+echo "Versión actual: $VERSION"
+
+# Incrementar según tipo
+if [ "$COMMIT_TYPE" = "feat" ]; then
+    MINOR=$((MINOR + 1))
+    PATCH=0
+    echo "Incrementando MINOR"
+elif echo "$COMMIT_TYPE" | grep -qE "^(fix|security|config|refactor|docs)$"; then
+    PATCH=$((PATCH + 1))
+    echo "Incrementando PATCH"
+else
+    echo "Tipo no reconocido, versión sin cambios"
+fi
+
+# Nueva versión
+NEW_VERSION="$MAJOR.$MINOR.$PATCH"
+TODAY=$(date +%d/%m/%Y)
+
+# Actualizar version.php
+sed -i "s/APP_VERSION', '[^']*'/APP_VERSION', '$NEW_VERSION'/" version.php
+sed -i "s/APP_UPDATE', '[^']*'/APP_UPDATE', '$TODAY'/" version.php
+
+echo "Versión actualizada a $NEW_VERSION"
