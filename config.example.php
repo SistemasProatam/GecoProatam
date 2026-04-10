0218405cf1ce0311a1bad0bfa5850393c7d5fd96
SistemasProatam <sistemas@proatam.com>
2026-04-09 19:24:29 -0500

    docs: agregar ejemplo de configuracion local
    Ejemplo del archivo config.php

---
+++ config.example.php
@@ -0,0 +1,12 @@
+<?php
+// Copia este archivo como config.php y ajusta tus credenciales locales (NO LO LLAMES CONFIG.EXAMPLE.PHP)
+// NUNCA subas config.php a GitHub
+
+$host = "nombre_del_host";
+$user = "nombre_usuario";
+$pass = "contraseña_duh";
+$db   = "nombre_db";
+
+// Desarrollo — muestra errores en desarrollo
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
